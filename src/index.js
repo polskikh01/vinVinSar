b0d5a2f86d6bd3086baf75f6b49ed889f9a4a453
Polskikh01 <polskikh01@gmail.com>
2022-04-26 22:43:44 +0400

    save theme in local storage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 const body = document.body;
 const darkMode = document.getElementById('darkMode');
-const allIcon = Array.from(document.querySelectorAll('img')).slice(0, 7);
+const allIcon = Array.from(document.querySelectorAll('img')).slice(0, 8);
+let theme = body.className === null ? '' : body.className;
+let setTheme;
+
+const iconTheme = () => {
+    body.className === '' ? allIcon.forEach(i => i.setAttribute('src', i.currentSrc.replace('dark', 'light'))) : allIcon.forEach(i => i.setAttribute('src', i.currentSrc.replace('light', 'dark')));
+}
 
 const lm = () => {
     body.classList.remove('darkMode');
     allIcon.forEach(i => i.setAttribute('src', i.currentSrc.replace('dark', 'light')));
+    theme = body.className;
+    localStorage.setItem('theme', theme);
 }
 
 const dm = () => {
     body.classList.add('darkMode');
     allIcon.forEach(i => i.setAttribute('src', i.currentSrc.replace('light', 'dark')));
+    theme = body.className;
+    localStorage.setItem('theme', theme);
 }
 
 darkMode.addEventListener('click', () => {
     body.className === '' ? dm() : lm();
 })
+
+setTheme = localStorage.getItem('theme');
+body.classList.add(String(setTheme));
+
+iconTheme();
